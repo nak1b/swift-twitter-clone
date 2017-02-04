a1aaeb45f2f13e765dda34fc739bdee74f4bcdb8
Nakib <nakib@Nakibs-MacBook-Pro.local>
2017-02-03 20:51:26 -0800

    setup to use code instead of storyboard

---
+++ twitter/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  twitter
+//
+//  Created by Nakib on 2/3/17.
+//  Copyright © 2017 Nakib. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.white
+    }
+}
